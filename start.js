b3ae821ad78656be90979167b7a394cef404f21c
D-A-W-N <Ichweisswasdugetanhastxd@googlemail.com>
2018-12-31 02:10:17 +0100

    Changed from "on Change" to "on Data" Event

--- start.js
+++ start.js
@@ -127,7 +127,7 @@ async function emitSensorData(io) {
         try {
             // erstelle `potentiometer` Hardwareinstanz.
             // [{ pin: 'A0' }, { pin: 'A1' }, { pin: 'A2' }, { pin: 'A3' }, { pin: 'A4' }]
-            var sensors = new five.Sensors([{ pin: 'A0', freq: 60 }, { pin: 'A1', freq: 60 }]);
+            var sensors = new five.Sensors([{ pin: 'A0', freq: 120 }, { pin: 'A1', freq: 120 }]);
             let i = 0;
 
             // Für jeden Sensor in der Collection werden Daten empfangen, bearbeitet und zum Webserver gesendet
@@ -138,7 +138,7 @@ async function emitSensorData(io) {
                 eval(p);
 
                 // Sofern sich die Werte ändern soll an den Webserver gesendet werden
-                sensor.on("change", function () {
+                sensor.on("data", function () {
                     // Skaliere ankommende Werte auf einen Bereich von -1 und 1
                     let v = this.fscaleTo([-1, 1]);
                     // Sende die Skalierten Werte an den Webserver
